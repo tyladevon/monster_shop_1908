1225307b63e683de3dddfe59f39897d84ad5cb4b
Meghan Stang <mstang15@gmail.com>
2019-10-21 07:55:02 -0600

    Add dependent destroys for merchant controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item <ApplicationRecord
   belongs_to :merchant
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :item_orders
   has_many :orders, through: :item_orders
 
