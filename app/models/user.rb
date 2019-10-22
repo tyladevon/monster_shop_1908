fa18a908953895cd7203aaf17aa5228eba57d87f
Ryan Hantak <rhantak@gmail.com>
2019-10-22 17:30:59 -0600

    Add validations for user attributes
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  validates_presence_of :name, :street_address, :city, :state, :zip
+  validates_presence_of :password, require: true
+  validates :email, uniqueness: true, presence: true
+  
   has_secure_password
-
 end
