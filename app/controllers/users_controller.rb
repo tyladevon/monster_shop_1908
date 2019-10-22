28b0f5dad53bacd524718e3873052cc9477d022a
Ryan Hantak <rhantak@gmail.com>
2019-10-22 17:31:54 -0600

    Handle incomplete registration forms and mistmatched passwords
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    flash[:success] = "You are now registered and logged in!"
-    session[:user_id] = user.id
-    redirect_to '/profile'
+    if user.save
+      flash[:success] = "You are now registered and logged in!"
+      session[:user_id] = user.id
+      redirect_to '/profile'
+    else
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to '/register'
+    end
   end
 
   def show
