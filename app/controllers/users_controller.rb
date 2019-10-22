c1f6b049e6d94bb79798d71fc4cfdd822c16080b
Tyla Devon <tyladevon@gmail.com>
2019-10-22 16:58:38 -0600

    Add MVC for user registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    user = User.create(user_params)
+    flash[:success] = "You are now registered and logged in!"
+    session[:user_id] = user.id
+    redirect_to '/profile'
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :street_address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
 end
