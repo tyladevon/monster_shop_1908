c676298877a29fd3cfb5423c6ea706241970cfd0
Ryan Hantak <rhantak@gmail.com>
2019-10-23 15:24:44 -0600

    Add create method to login an existing user to the session
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,26 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
   def create
-  
+    user = User.find_by(email: login_params[:email])
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      if user.reg?
+        redirect_to '/profile'
+      else
+        redirect_to '/dashboard'
+      end
+    else
+      flash.now[:error] = "Email and password do not match."
+      render :new
+    end
   end
+
+  private
+    def login_params
+      params.permit(:email, :password)
+    end
 end
