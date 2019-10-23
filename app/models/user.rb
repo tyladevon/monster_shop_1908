4087b18349abd674d4e2abb95e96f97e73108f7e
Ryan Hantak <rhantak@gmail.com>
2019-10-23 13:32:46 -0600

    Add enumerator for role attribute
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates_presence_of :name, :street_address, :city, :state, :zip
   validates_presence_of :password, require: true
   validates :email, uniqueness: true, presence: true
-  
+
   has_secure_password
+
+  enum role: %w(reg admin merch_employee merch_admin)
 end
