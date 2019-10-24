ec1d73f75016f76d9cfc081781b6483b69d72e18
Tyla Devon <tyladevon@gmail.com>
2019-10-23 20:21:52 -0600

    Add test for admin user nav.

---
+++ spec/features/navigation/admin_user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "As an Admin User" do
+  before(:each) do
+    @admin_user = User.create(name: "Bob G",
+                        street_address: "123 Avenue",
+                        city: "Portland",
+                        state: "OR",
+                        zip: "30203",
+                        email: "bobg@agency.com",
+                        password: "bobg",
+                        password_confirmation: "bobg",
+                        role: 1
+                        )
+
+  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_user)
+  visit '/'
+end
+
+  it "can see links just as regular user" do
+    expect(page).to have_link "All Items"
+    expect(page).to have_link "All Merchants"
+    expect(page).to have_link "Home"
+    expect(page).to have_link "Profile"
+    expect(page).to have_link "Logout"
+  end
+
+  it "can see admin links" do
+    expect(page).to have_link "Admin Dashboard"
+    expect(page).to have_link "All Users"
+
+  end
+
+  it "cannot see link for shopping cart" do
+    expect(page).to_not have_content("Cart")
+  end
+end
+#
+# Minus the following links/info
+# - a link to my shopping cart ("/cart") or count of cart items
