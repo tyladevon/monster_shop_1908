06666b1f05eaa77cb452b96b79c67d6d4df4dfcb
Tyla Devon <tyladevon@gmail.com>
2019-10-24 13:31:46 -0600

    Add test for user edit password. Not complete

---
+++ spec/features/users/regular_user_edit_password_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Regular user visit profile page" do
+  describe "can edit password" do
+    it "can see link and fill in new password" do
+      user = User.create(name: "Dee",
+                         street_address: "4233 Street",
+                         city: "Golden",
+                         state: "CO",
+                         zip: "80042",
+                         email: "deedee@gmail.com",
+                         password: "rainbows1908",
+                         role: 0)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit '/profile'
+
+    click_link "Edit Password"
+    expect(current_path).to eq("/user/#{user.id}/edit")
+
+    fill_in :new_password, with: "Rainbows1900"
+    fill_in :new_password_confirmation, with: "Rainbows1900"
+
+    click_button "Submit"
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content("Your password has been updated!")
+    end
+  end
+end
