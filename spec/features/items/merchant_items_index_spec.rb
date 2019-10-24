9f2058e2398d7b511be3963aa0093255536018f0
Tyla Devon <tyladevon@gmail.com>
2019-10-24 11:29:36 -0600

    Refactor merchant items index test to include admin user

--- spec/features/items/merchant_items_index_spec.rb
+++ spec/features/items/merchant_items_index_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe "Merchant Items Index Page" do
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      @user = User.create(name: "Dee",
+                         street_address: "4233 Street",
+                         city: "Golden",
+                         state: "CO",
+                         zip: "80042",
+                         email: "deedee@gmail.com",
+                         password: "rainbows1908",
+                         role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
     end
 
     it 'shows me a list of that merchants items' do
