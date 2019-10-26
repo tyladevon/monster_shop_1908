813605ad84c9b09b860a8e1d21ced74ac894122d
Ryan Hantak <rhantak@gmail.com>
2019-10-26 11:18:55 -0600

    Add migrate to add status column to orders table
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191025165354) do
+ActiveRecord::Schema.define(version: 20191026164946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20191025165354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
