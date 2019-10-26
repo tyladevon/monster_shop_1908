2a009f32dc0693e2a36de95adda978d1e75515d3
Ryan Hantak <rhantak@gmail.com>
2019-10-26 11:17:41 -0600

    Add test scenario for a user visiting a populated my orders page
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

---
+++ spec/features/orders/profile_orders_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "As a registered user" do
+  describe "I visit my profile orders page" do
+    it "I see details of every order I've made" do
+      @user = User.create(
+        name: "Profile User",
+        street_address: "345 Blvd",
+        city: "San Antonio",
+        state: "Texas",
+        zip: "60789",
+        email: "profile@gmail.com",
+        password: "pass",
+        password_confirmation: "pass",
+        role: 0
+      )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @order_1 = @user.orders.create(name: "Reg", address: "123 Street", city: "Denver", state: "CO", zip: "80202", user_id: @user.id)
+      ItemOrder.create(order_id: @order_1.id, item_id: @paper.id, price: 20, quantity: 2)
+      ItemOrder.create(order_id: @order_1.id, item_id: @pencil.id, price: 2, quantity: 2)
+
+      visit '/profile/orders'
+
+      within "#order-#{@order_1.id}" do
+        expect(page).to have_link(@order_1.id)
+        expect(page).to have_content(@order_1.created_at)
+        expect(page).to have_content(@order_1.updated_at)
+        expect(page).to have_content(@order_1.status)
+        expect(page).to have_content(@order_1.total_items)
+        expect(page).to have_content(@order_1.grandtotal)
+      end
+    end
+  end
+end
