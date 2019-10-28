c2b386801e6b5abe615fa57b56cddded1e3cda57
Tyla Devon <tyladevon@gmail.com>
2019-10-28 16:50:54 -0600

    Add functionality to change order status in update action.
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -1,8 +1,12 @@
-class ItemOrdersController < ApplicationController 
+class ItemOrdersController < ApplicationController
+
   def update
     item_order = ItemOrder.find(params[:id])
     item_order.update(fulfilled: true)
     item_order.item.update_attributes(:inventory => (item_order.item.inventory - item_order.quantity))
+    if ItemOrder.where(fulfilled: false).empty?
+      item_order.order.update(status: "Packaged")
+    end
     flash[:notice] = "This item has been fulfilled - #{item_order.item.name}"
     redirect_to "/merchant/orders/#{item_order.order.id}"
   end
