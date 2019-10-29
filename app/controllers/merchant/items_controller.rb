b84f90782643d14320d0a7531baa46acd13128b6
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-29 15:26:52 -0600

    Add update action in Merchant::ItemsController

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,8 +1,18 @@
 class Merchant::ItemsController < Merchant::BaseController
 
-
   def index
     @items = Item.where(merchant_id: current_user.merchant_id)
   end
 
+  def update 
+    item = Item.find(params[:id])
+    if params["type"] == "deactivate"
+      item.update(active?: false)
+      flash[:notice] = "#{item.name} is no longer for sale"
+    elsif params["type"] == "activate"
+      item.update(active?: true)
+      flash[:notice] = "#{item.name} is available for sale"
+    end
+    redirect_to '/merchant/items'
+  end
 end
