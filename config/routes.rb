02d3502ce27d2926b46d78523431a0329a161087
Ryan Hantak <rhantak@gmail.com>
2019-10-30 15:18:10 -0600

    Add route for admin users index page

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/', to: 'dashboard#index'
+    get '/users', to: 'users#index'
     patch '/orders/:id', to: 'orders#update'
   end
 
