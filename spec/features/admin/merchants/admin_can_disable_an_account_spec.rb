8febc85cb7de37e98c8ba111a533e6b36b12f482
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-30 20:06:20 -0600

    Update disable merchant spec to include items

--- spec/features/admin/merchants/admin_can_disable_an_account_spec.rb
+++ spec/features/admin/merchants/admin_can_disable_an_account_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'As an admin user' do
   describe 'When I visit the merchants index page at /merchants' do
-    it "I see all merchants in the system" do
+    it "I click the disable button to disable a Merchant's account and their items" do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)      
       admin_user = User.create(
         name: "Mike",
@@ -18,6 +18,9 @@ RSpec.describe 'As an admin user' do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
       bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      yellow_pencil = bike_shop.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      blue_pencil = bike_shop.items.create(name: "Blue Pencil", description: "You can write on paper with it!", price: 3, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 50)
+      green_pencil = bike_shop.items.create(name: "Green Pencil", description: "You can write on paper with it!", price: 4, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 20, active?: false)
 
       visit "/admin/merchants"
 
@@ -31,6 +34,13 @@ RSpec.describe 'As an admin user' do
       within "#merchant-#{bike_shop.id}" do
         expect(page).to have_button("Enable")
       end
+
+      item_1 = Item.find(yellow_pencil.id)
+      item_2 = Item.find(blue_pencil.id)
+      item_3 = Item.find(green_pencil.id)
+      expect(item_1.active?).to eq(false)
+      expect(item_1.active?).to eq(false)
+      expect(item_1.active?).to eq(false)
     end
   end
 end
