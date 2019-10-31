1b89b70735c4ad304867cce98f773d34f3d876b4
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-30 19:15:23 -0600

    Add update action to Admin::MerchantsController

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,16 @@ class Admin::MerchantsController < Admin::BaseController
   def index
     @merchants = Merchant.all
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if params[:type] == "enable"
+      merchant.update(enabled?: true)
+      flash[:notice] = "#{merchant.name} account is enabled."
+    elsif params[:type] == "disable"
+      merchant.update(enabled?: false)
+      flash[:notice] = "#{merchant.name} account is disabled."
+    end
+    redirect_to '/admin/merchants'
+  end
 end
