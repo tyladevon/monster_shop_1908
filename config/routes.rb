fdeffb4105b4967ae41866f6b94ab7e9267d5087
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-30 19:14:51 -0600

    Add route for Admin::Merchants update action

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/', to: 'dashboard#index'
     get '/merchants', to: 'merchants#index'
+    patch '/merchants/:id/:type', to: 'merchants#update'
   end
 
   namespace :merchant do
