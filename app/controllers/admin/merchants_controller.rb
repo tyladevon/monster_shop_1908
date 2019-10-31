fcabc9a3e5a824740e8856f839b41a2709321dc0
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-30 20:08:06 -0600

    Add enabling/disabling merchant items to update

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,18 @@ class Admin::MerchantsController < Admin::BaseController
   def index
     @merchants = Merchant.all
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if params[:type] == "enable"
+      merchant.update(enabled?: true)
+      merchant.items.update_all(active?: true)
+      flash[:notice] = "#{merchant.name} account is enabled."
+    elsif params[:type] == "disable"
+      merchant.update(enabled?: false)
+      merchant.items.update_all(active?: false)
+      flash[:notice] = "#{merchant.name} account is disabled."
+    end
+    redirect_to '/admin/merchants'
+  end
 end
