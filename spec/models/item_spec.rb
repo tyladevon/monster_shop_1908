ba356c30c5907f06d90657fbeeae73ff5f640863
Tyla Devon <tyladevon@gmail.com>
2019-10-30 22:01:16 -0600

    Add testing content for class method test for item stats.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,6 +27,7 @@ describe Item, type: :model do
       @review_3 = @chain.reviews.create(title: "Meh place", content: "They have meh bike stuff and I probably won't come back", rating: 1)
       @review_4 = @chain.reviews.create(title: "Not too impressed", content: "v basic bike shop", rating: 2)
       @review_5 = @chain.reviews.create(title: "Okay place :/", content: "Brian's cool and all but just an okay selection of items", rating: 3)
+
     end
 
     it "calculate average review" do
@@ -39,14 +40,6 @@ describe Item, type: :model do
 
       expect(top_three).to eq([@review_1,@review_2,@review_5])
       expect(bottom_three).to eq([@review_3,@review_4,@review_5])
-    end
-
-    it 'top_five_most_popular' do
-      
-    end
-
-    it 'least_popular' do
-
     end
 
     it 'no orders' do
@@ -66,5 +59,53 @@ describe Item, type: :model do
       order.item_orders.create(item: @chain, price: @chain.price, quantity: 2)
       expect(@chain.no_orders?).to eq(false)
     end
+  end
+
+  describe "class methods" do
+    before(:each) do
+      @user = User.create(
+        name: "Profile User",
+        street_address: "345 Blvd",
+        city: "San Antonio",
+        state: "Texas",
+        zip: "60789",
+        email: "profile@gmail.com",
+        password: "pass",
+        password_confirmation: "pass",
+        role: 0
+      )
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 5, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 100)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @pen = @mike.items.create(name: "Black Pen", description: "You can write!", price: 4, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @binder = @mike.items.create(name: "Binder", description: "You can hold papers!", price: 8, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @ruler = @mike.items.create(name: "Ruler", description: "You can write measure!", price: 3, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @wide_rule = @mike.items.create(name: "Wide Rule Paper", description: "You can write on it!", price: 4, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @college_rule = @mike.items.create(name: "College Rule Paper", description: "You can write on it!", price: 4, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @divider = @mike.items.create(name: "Colorful Dividers", description: "You can devide!", price: 4, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @folder = @mike.items.create(name: "Simple Folder", description: "You can organize with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @tape = @mike.items.create(name: "Tape", description: "You can tape with it!", price: 5, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+      @order_1 = @user.orders.create(name: "Reg", address: "123 Street", city: "Denver", state: "CO", zip: "80202", user_id: @user.id)
+      ItemOrder.create(order_id: @order_1.id, item_id: @paper.id, price: 5, quantity: 6)
+      ItemOrder.create(order_id: @order_1.id, item_id: @pencil.id, price: 2, quantity: 4)
+      ItemOrder.create(order_id: @order_1.id, item_id: @pen.id, price: 4, quantity: 7)
+      ItemOrder.create(order_id: @order_1.id, item_id: @binder.id, price: 8, quantity: 2)
+      ItemOrder.create(order_id: @order_1.id, item_id: @folder.id, price: 2, quantity: 10)
+      ItemOrder.create(order_id: @order_1.id, item_id: @ruler.id, price: 3, quantity: 1)
+      ItemOrder.create(order_id: @order_1.id, item_id: @wide_rule.id, price: 4, quantity: 5)
+      ItemOrder.create(order_id: @order_1.id, item_id: @college_rule.id, price: 4, quantity: 9)
+      ItemOrder.create(order_id: @order_1.id, item_id: @divider.id, price: 4, quantity: 8)
+      ItemOrder.create(order_id: @order_1.id, item_id: @tape.id, price: 5, quantity: 3)
+
+    end
+  end
+  it 'top_five_most_popular' do
+    expect(Item.top_five_most_popular).to eq([@folder, @college_rule, @divider, @pen, @paper])
+  end
+
+  it 'least_popular' do
+    expect(Item.least_popular).to eq([@wide_rule, @pencil, @tape, @binder, @ruler])
   end
 end
